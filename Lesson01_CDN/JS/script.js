5b711ebd5b134fb9b9819446a10d0d9703712f1f
Hạnh Mai <hanhmai120997@gmail.com>
2025-09-01 23:13:20 +0700

    Create script.js

---
+++ Lesson01_CDN/JS/script.js
@@ -0,0 +1,9 @@
+// 2. scripts.js
+var scripts = [
+    "unpkg.com/react@18/umd/react.development.js",
+    "unpkg.com/react-dom@18/umd/react-dom.development.js",
+    "unpkg.com/@babel/standalone/babel.min.js"
+];
+for(var i in scripts){
+    document.write('<script crossorigin src="https://' + scripts[i] + '"></script>');
+};
